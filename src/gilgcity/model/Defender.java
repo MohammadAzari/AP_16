c3c15f2a402562d35c480cfb7aefbad58955959b
sara <sara.rajabzadeh.1999@gmail.com>
2018-10-25 16:43:12 +0330

    setter getter added for Defender

--- src/gilgcity/model/Defender.java
+++ src/gilgcity/model/Defender.java
@@ -2,25 +2,87 @@ package gilgCity.model;
 
 public class Defender {
 
-    int level;
-    int blockId;
-    int unitId;
-    double score;
-    static int dayLives;
-    int employees;
-    double cost;
+    private int level;
+    private int blockId;
+    private int unitId;
+    private double score;
+    private static int dayLives;
+    private int employees;
+    private static double cost;
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setBlockId(int blockId) {
+        this.blockId = blockId;
+    }
+
+    public void setUnitId(int unitId) {
+        this.unitId = unitId;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public static void setDayLives(int dayLives) {
+        Defender.dayLives = dayLives;
+    }
+
+    public void setEmployees(int employees) {
+        this.employees = employees;
+    }
+
+    public static void setCost(double cost) {
+        Defender.cost = cost;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getBlockId() {
+        return blockId;
+    }
+
+    public int getUnitId() {
+        return unitId;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public static int getDayLives() {
+        return dayLives;
+    }
+
+    public int getEmployees() {
+        return employees;
+    }
+
+    public static double getCost() {
+        return cost;
+    }
 
     public Defender(){
         dayLives = 0;
 
     }
+
+
+
     public static void upgrade(){
 
-
+        cost = cost - 5000;
 
     }
 
+
     public static void remove(){
+
+        cost += 10000;
 
 
     }
