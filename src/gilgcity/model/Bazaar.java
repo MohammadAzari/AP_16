77b3a61fd590c00d652cebd118a4aec4b43bab64
elie <masoumi.elnaz2@gmail.com>
2018-10-25 18:08:37 +0330

    Changing Bazaar

--- src/gilgcity/model/Bazaar.java
+++ src/gilgcity/model/Bazaar.java
@@ -2,9 +2,59 @@ package gilgCity.model;
 
 public class Bazaar {
 
-    double score;
-    int level;
-    int numberOfplayers;
+    public static double score;
+    public double cost;
+    public int level;
+    public int numberOfplayers;
+    public int blockId;
+    public int unitId;
+    public int dayExist;
+
+
+
+    public Bazaar(int level, int numberOfplayers) {
+        this.level = level;
+        this.numberOfplayers=numberOfplayers;
+    }
+
+//    public int cost_calculate(double cost,int level){
+//        cost=(level+1)*5000;
+//        return cost;
+//    }
+
+    public void upgrade(int level,double score,int unitId,int numberOfplayers) {
+
+        if (level == 2) {
+            score = numberOfplayers*(score*1.4);
+        }
+        if(level == 3){
+            score=numberOfplayers*(score*1.6);
+        }
+
+
+    }
+
+    public void remove(int level,double score,int numberOfplayers){
+        if(level == 2){
+            score=score*numberOfplayers/1.4;
+        }
+        if(level == 3){
+            score=score*numberOfplayers/1.6;
+        }
+
+
+    }
+
+    public void addDaysLive(){
+
+    }
+
+
+
+
+
+
+
 
 
 }
