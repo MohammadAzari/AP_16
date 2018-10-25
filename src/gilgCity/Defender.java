a3fb983c7d8bfabd94d954c6e09632d8e09c1b67
sara <sara.rajabzadeh.1999@gmail.com>
2018-10-25 14:56:02 +0330

    Defender class added

---
+++ src/gilgCity/Defender.java
@@ -0,0 +1,35 @@
+package gilgCity;
+
+public class Defender {
+
+    int level;
+    int blockId;
+    int unitId;
+    double score;
+    static int dayLives;
+    int employees;
+    double cost;
+
+    public Defender(){
+        dayLives = 0;
+
+    }
+    public static void upgrade(){
+
+
+
+    }
+
+    public static void remove(){
+
+
+    }
+
+    public static void addDayLives(){
+
+        dayLives++;
+    }
+
+
+
+}
