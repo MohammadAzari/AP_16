36cd1d354c5142f2c4a53dd31eea03e3563fc9fe
elie <masoumi.elnaz2@gmail.com>
2018-10-26 15:22:16 +0330

    Constants of Game added!

--- src/gilgCity/model/User.java
+++ src/gilgCity/model/User.java
@@ -1,15 +1,63 @@
 package gilgCity.model;
 
+import gilgCity.controller.counter;
+
 public class User {
-    private double score;
+    private double score=0L;
+    private double baseScore;
+    private counter counter=new counter();
     private double gills;
-    private City city;
+    private City city=new City();
+    private boolean turn=false;
+    private String name;
+
+    public boolean turn() {
+        return turn;
+    }
+
+    public void turn(boolean turn) {
+        turn = turn;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public double getBaseScore() {
+        return baseScore;
+    }
+
+    public void setBaseScore(double baseScore) {
+        this.baseScore = baseScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public void showScore(){
+
         System.out.println(this.score);
     }
 
     public void showGills(){
+
         System.out.println(this.gills);
     }
 
