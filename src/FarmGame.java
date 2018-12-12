ffc13390e828b4fd7a41e5c0859f32c560a13f83
Mohammad Azari <mazard@protonmail.com>
2018-12-12 17:25:10 +0330

    Adding Animal Class

--- src/FarmGame.java
+++ src/FarmGame.java
@@ -84,11 +84,72 @@ class Map{
     }
 }
 
-abstract class Animal {
+interface Moveable{
+    void move();
+}
+class Animal implements Moveable{
+    private Position position = new Position();
+    private Map map;
+    private Random random = new Random();
+    private Constants constants = new Constants();
 
-    double price;
-    //map
+    public Animal(Map map){
+        this.map = map;
+        position.setX(random.nextInt(map.getSize()));
+        position.setY(random.nextInt(map.getSize()));
+    }
 
+    public void move(){
+        switch (random.nextInt(map.getSize())){
+
+            case Constants.EAST:
+                if (position.getX() + 1 < map.getSize()) {
+                    position.setX(position.getX() + 1);
+                    map.printMap();
+                }
+                else {
+                    position.setX(position.getX() - 1);
+                    map.printMap();
+                }
+                break;
+
+            case Constants.NORTH:
+                if (position.getY() + 1 < map.getSize()) {
+                    position.setY(position.getY() + 1);
+                    map.printMap();
+                }
+                else {
+                    position.setY(position.getY() - 1);
+                    map.printMap();
+                }
+                break;
+
+            case Constants.SOUTH:
+                if (position.getY() - 1 < map.getSize()) {
+                    position.setY(position.getY() - 1);
+                    map.printMap();
+                }
+                else {
+                    position.setY(position.getY() + 1);
+                    map.printMap();
+                }
+                break;
+
+            case Constants.WEST:
+                if (position.getX() - 1 < 0){
+                    position.setX(position.getX() + 1);
+                    map.printMap();
+                }
+                else {
+                    position.setX(position.getX() - 1);
+                    map.printMap();
+                }
+                break;
+
+            default: break;
+
+        }
+    }
 }
 
 class Wild extends Animal{
